ab5fe2f4e5b5a0e03c91816ce72dd80b6126608b
Ben Macleod <benstreasurebox@gmail.com>
2023-05-09 20:56:23 -0400

    feat: added chakra to each app

--- apps/comm.frms.io/src/pages/_app.tsx
+++ apps/comm.frms.io/src/pages/_app.tsx
@@ -1,9 +1,13 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { type AppType } from "next/dist/shared/lib/utils";
-
-import "~/styles/globals.css";
+import { theme } from "styles";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+	return (
+		<ChakraProvider resetCSS theme={theme}>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
 };
 
 export default MyApp;
